3ea6d0a36524227802a1113bab68c8aaa148c3a9
JackieW001 <jwoo@stuy.edu>
2018-04-14 22:13:19 -0400

    fix stack

---
+++ main.py
@@ -0,0 +1,18 @@
+from display import *
+from draw import *
+from parser import *
+from matrix import *
+import math
+
+screen = new_screen()
+color = [ 0, 255, 0 ]
+edges = []
+polygons = []
+
+t = new_matrix() 
+ident(t)
+stack = [t]
+
+transform = new_matrix()
+
+parse_file( 'script', stack, edges, polygons, transform, screen, color )
